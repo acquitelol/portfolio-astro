f9f7a83e345e88117edda8d7b301927df3ee3b86
Acquite <acquitefx@gmail.com>
2022-08-21 15:39:11 +0100

    made nav uppercase and changed button

--- portfolio-astro/src/components/navbar/navbar.tsx
+++ portfolio-astro/src/components/navbar/navbar.tsx
@@ -7,10 +7,23 @@ interface containerProps {
 
 const Navbar: React.FC<containerProps> = ({navbarArray}) => {
     return (
-        navbarArray.map((navBarItem, index) => {
+        navbarArray.map((navBarItem, index: number) => {
+            const getName = (string: string) => {
+                let name = string
+                let formatted = name.slice(0).replaceAll('%20', ' ')
+              
+                if (formatted==='') return "Invalid"
+              
+                let one = formatted.split('', 1)
+                let two = [one[0], formatted.slice(1)]
+                one[0] = one[0].toUpperCase();
+                let final = one[0]+two[1]
+              
+                return final
+            }
             return (
                 <div index={index} className="inline">
-                    <a href={navBarItem.url}>{navBarItem.name}</a>
+                    <a href={navBarItem.url}>{getName(navBarItem.name)}</a>
                 </div>
             )
         })
